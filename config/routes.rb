0c5bc11837df7d10ffb8c2bb3a1f2fa46f76d4a5
bryan.knouse@gmail.com <bryan.knouse@gmail.com>
2019-01-14 19:20:54 -0500

    added assets and polished style transfer

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   get 'main/index'
 
   root 'main#index'
+
+  match '/owldetails', to: 'main#owldetails', via: 'get'
+  match '/clusterduck', to: 'main#clusterduck', via: 'get'
+  match '/opensource', to: 'main#opensource', via: 'get'
   
 end
